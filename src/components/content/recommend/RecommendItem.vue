caf6cc5a2d3c2a06ce861f09aca4bc82890f6193
Tcgogo <1812497229@qq.com>
2020-11-23 23:51:09 +0800

    初步完成recommend

---
+++ src/components/content/recommend/RecommendItem.vue
@@ -0,0 +1,97 @@
+<!--  -->
+<template>
+  <div class="recommend-list">
+    <div class="recommend-img">
+      <img
+        src="http://img1.qunarzz.com/sight/p0/1507/52/0e7cb0d161c785b41bab3664bf8c692a.water.jpg_200x200_e3f7a99e.jpg"
+        alt=""
+      />
+    </div>
+    <div class="recommend-detail">
+      <p>{{ recommend.title }}</p>
+      <div>
+        <span class="iconfont icon-pingfen" v-for="i of 5" :key="i"></span>
+        <span class="pinglun">{{ recommend.pinglun }}条评论</span>
+      </div>
+      <div class="message">
+        <span class="price">{{ recommend.price | price }}</span>
+        <span class="site">{{ recommend.site }}</span>
+      </div>
+      <div class="decription">{{ recommend.decription }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    recommend: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  filters: {
+    price(num) {
+      if(!isNaN(Number(num))){
+        return "￥" + num;
+      }
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.recommend-list {
+  display: flex;
+  margin-left: 0.75rem;
+  padding: 0.625rem 0;
+  .recommend-img {
+    width: 6.25rem;
+    img {
+      width: 100%;
+    }
+  }
+
+  .recommend-detail {
+    padding-left: 0.8rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-width: 0;
+    p {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    .iconfont {
+      color: #ffb436;
+    }
+    .pinglun {
+      margin-left: 0.75rem;
+      font-size: 0.75rem;
+    }
+    .message {
+      display: flex;
+    }
+
+    .price {
+      color: rgb(245, 118, 15);
+      flex: 1;
+      margin-right: auto;
+    }
+
+    .site {
+      font-size: 0.75rem;
+    }
+
+    .decription {
+      color: #ff5555;
+      font-size: 0.75rem;
+    }
+  }
+}
+</style>
--- src/views/home/childComponents/Recommend.vue
+++ src/views/home/childComponents/Recommend.vue
@@ -1,37 +1,50 @@
 <template>
   <div class="recommend">
     <div class="recommend-title"><i class="iconfont icon-xin"></i>猜你喜欢</div>
-    <div class="recommend-list">
-      <div class="recommend-img">
-        <img
-          src="http://img1.qunarzz.com/sight/p0/1507/52/0e7cb0d161c785b41bab3664bf8c692a.water.jpg_200x200_e3f7a99e.jpg"
-          alt=""
-        />
-      </div>
-      <div class="recommend-detail">
-        <p>水濂山乐民游艺园</p>
-        <div>
-          <span class="iconfont icon-pingfen" v-for="i of 5" :key="i"></span>
-          <span class="pinglun">{{ num }}条评论</span>
-        </div>
-        <div class="message">
-          <span class="price">￥250</span>
-          <span class="site">广州长隆</span>
-        </div>
-        <div class="decription">
-          爱挑战的你怎能错过垂直过山车
-        </div>
-      </div>
-    </div>
+    <recommend-item
+      v-for="item of recommendList"
+      :key="item.id"
+      :recommend="item"
+    ></recommend-item>
   </div>
 </template>
 
 <script>
+import RecommendItem from "@/components/content/recommend/RecommendItem";
+
 export default {
   data() {
     return {
-      num: 100,
+      recommendList: [
+        {
+          id: "0001",
+          title: "水濂山乐民游艺园",
+          pinglun: "11245",
+          price: 250,
+          site: "长隆",
+          decription: "爱挑战的你怎能错过垂直过山车",
+        },
+        {
+          id: "0001",
+          title: "水濂山乐民游艺园",
+          pinglun: "11245",
+          price: 250,
+          site: "长隆",
+          decription: "爱挑战的你怎能错过垂直过山车",
+        },
+        {
+          id: "0001",
+          title: "水濂山乐民游艺园",
+          pinglun: "11245",
+          price: 250,
+          site: "长隆",
+          decription: "爱挑战的你怎能错过垂直过山车",
+        },
+      ],
     };
+  },
+  components: {
+    RecommendItem,
   },
 };
 </script>
@@ -44,50 +57,6 @@ export default {
     .iconfont {
       color: red;
       font-size: 1.3rem;
-    }
-  }
-
-  .recommend-list {
-    display: flex;
-    margin-left: 0.75rem;
-    padding: .625rem 0;
-    .recommend-img {
-      width: 6.25rem;
-      img {
-        width: 100%;
-      }
-    }
-
-    .recommend-detail {
-      padding-left: 0.8rem;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      .iconfont {
-        color: #ffb436;
-      }
-      .pinglun {
-        margin-left: .75rem;
-        font-size: 0.75rem;
-      }
-      .message {
-        display: flex;
-      }
-
-      .price{
-        color: rgb(245, 118, 15);
-        flex: 1;
-        margin-right: auto;
-      }
-
-      .site{
-        font-size: .75rem;
-      }
-
-      .decription {
-        color: #ff5555;
-        font-size: .75rem;
-      }
     }
   }
 }
